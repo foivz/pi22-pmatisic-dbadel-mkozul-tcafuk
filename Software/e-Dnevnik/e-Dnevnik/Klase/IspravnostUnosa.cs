08706de597215cdc4a2710aa2daf01700585ac19
Petar Matišić <pmatisic@foi.hr>
2022-07-05 03:11:33 +0200

    dodane klase

---
+++ Software/e-Dnevnik/e-Dnevnik/Klase/IspravnostUnosa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Dnevnik.Klase
+{
+    public class IspravnostUnosa : ApplicationException
+    {
+        public string Poruka { get; set; }
+        public IspravnostUnosa(string poruka)
+        {
+            Poruka = poruka;
+        }
+    }
+}

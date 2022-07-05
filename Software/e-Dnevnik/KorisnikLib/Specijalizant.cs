f162a2b8809f2e1041a3c72035ff76105a8d5cea
Petar Matišić <pmatisic@foi.hr>
2022-07-05 03:19:11 +0200

    maknuto i dodano

---
+++ Software/e-Dnevnik/KorisnikLib/Specijalizant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorisnikLib
+{
+    public class Specijalizant : Korisnik
+    {
+        public string Titula { get; set; }
+        //public Image Slika { get; set; }
+        public int ID_specijalizant { get; set; }
+
+        public Specijalizant(int id, string ime, string prezime, string korisnickoIme, string lozinka, string email) : base(ime, prezime, korisnickoIme, lozinka, email, Uloga.Specijalizant)
+        {
+            ID_specijalizant = id;
+        }
+
+        public override string ToString()
+        {
+            return Ime + " " + Prezime + " <" + KorisnickoIme + "> ";
+        }
+    }
+}

ee668de0d82185ceb80bf5fb1778f2cee7b047b6
Dorian Badel <DorianBadel@protonmail.com>
2022-07-05 21:38:18 +0100

    Izbor izmedu razlicitih dogadaja

--- Software/e-dnevnik/e-Dnevnik/frmDogadaji.cs
+++ Software/e-dnevnik/e-Dnevnik/frmDogadaji.cs
@@ -17,12 +17,13 @@ namespace e_Dnevnik
         {
             this.mainForm = mainForm;
             InitializeComponent();
+            btnProvjereZnanja.Enabled = false;
+            dgvDogađaji.DataSource = GetProvjereZnanja();
         }
 
         private void frmDogadaji_Load(object sender, EventArgs e)
         {
-            dgvDogađaji.DataSource = GetProvjereZnanja();
-            btnProvjereZnanja.Enabled = false;
+            paintRows();
         }
 
         private object GetProvjereZnanja()
@@ -39,7 +40,6 @@ namespace e_Dnevnik
                            && d.Korisnik_idKorisnik == k.idKorisnik
                            && d.TipDogadjaja_idTipDogadjaja == t.idTipDogadjaja
                            && d.TipDogadjaja_idTipDogadjaja == 1
-
                            select new
                            {
                                ID_provjere = pz.idProvjeraZnanja,
@@ -67,6 +67,35 @@ namespace e_Dnevnik
             btnProvjereZnanja.Enabled = true;
             btnDnevneAktivnosti.Enabled = true;
             btnSlucajeviBolesnika.Enabled = false;
+            dgvDogađaji.DataSource = GetSlucajeviBolesnika();
+            paintRows();
+
+        }
+
+        private object GetSlucajeviBolesnika()
+        {
+            using (var context = new PI2205_DBEntities())
+            {
+                var upit = from sb in context.SlucajBolesnika
+                           from d in context.Dogadjaj
+                           from t in context.TipDogadjaja
+                           from k in context.Korisnik
+                           where k.ProgramSpecijalizacije_idProgramSpecijalizacije == 1
+                           && d.idDogadjaj == sb.Dogadjaj_idDogadjaj
+                           && d.Korisnik_idKorisnik == k.idKorisnik
+                           && d.TipDogadjaja_idTipDogadjaja == t.idTipDogadjaja
+                           && d.TipDogadjaja_idTipDogadjaja == 2
+
+                           select new
+                           {
+                               ID_slucaja = sb.idSlucajBolesnika,
+                               Naziv_dogadaja = d.nazivdogadjaja,
+                               Status = d.statusdogadjaja,
+                               Tip = t.nazivtipa,
+                               Korisnik = k.korime
+                           };
+                return upit.ToList();
+            }
         }
 
         private void btnDnevneAktivnosti_Click(object sender, EventArgs e)
@@ -74,6 +103,34 @@ namespace e_Dnevnik
             btnProvjereZnanja.Enabled = true;
             btnDnevneAktivnosti.Enabled = false;
             btnSlucajeviBolesnika.Enabled = true;
+            dgvDogađaji.DataSource = GetDnevnikAktivnosti();
+            paintRows();
+        }
+
+        private object GetDnevnikAktivnosti()
+        {
+            using (var context = new PI2205_DBEntities())
+            {
+                var upit = from da in context.DnevnaAktivnost
+                           from d in context.Dogadjaj
+                           from t in context.TipDogadjaja
+                           from k in context.Korisnik
+                           where k.ProgramSpecijalizacije_idProgramSpecijalizacije == 2
+                           && d.idDogadjaj == da.Dogadjaj_idDogadjaj
+                           && d.Korisnik_idKorisnik == k.idKorisnik
+                           && d.TipDogadjaja_idTipDogadjaja == t.idTipDogadjaja
+                           && d.TipDogadjaja_idTipDogadjaja == 3
+
+                           select new
+                           {
+                               ID_dnevne = da.idDnevnaAktivnost,
+                               Naziv_dogadaja = d.nazivdogadjaja,
+                               Status = d.statusdogadjaja,
+                               Tip = t.nazivtipa,
+                               Korisnik = k.korime
+                           };
+                return upit.ToList();
+            }
         }
 
         private void btnProvjereZnanja_Click(object sender, EventArgs e)
@@ -81,6 +138,19 @@ namespace e_Dnevnik
             btnProvjereZnanja.Enabled = false;
             btnDnevneAktivnosti.Enabled = true;
             btnSlucajeviBolesnika.Enabled = true;
+            dgvDogađaji.DataSource = GetProvjereZnanja();
+            paintRows();
+        }
+
+        private void paintRows()
+        {
+            foreach (DataGridViewRow row in dgvDogađaji.Rows)
+            {
+                if (row.Cells[2].Value.ToString() == "Pao rok" || row.Cells[2].Value.ToString() == "")
+                {
+                    row.DefaultCellStyle.BackColor = Color.LightGray;
+                }
+            }
         }
     }
 }

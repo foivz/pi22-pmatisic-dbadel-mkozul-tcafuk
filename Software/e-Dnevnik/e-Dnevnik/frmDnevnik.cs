bdb014e09c05d0bcc99930ed44ccc990de672cd2
Mihael Kožul <mkozul@foi.hr>
2022-07-06 21:36:52 +0200

    ispis dnevnika, biljeske

--- Software/e-Dnevnik/e-Dnevnik/frmDnevnik.cs
+++ Software/e-Dnevnik/e-Dnevnik/frmDnevnik.cs
@@ -21,27 +21,50 @@ namespace e_Dnevnik
             entities.DnevnaAktivnost.Load();
             entities.ProvjeraZnanja.Load();
             entities.Biljeske.Load();
+            entities.Dogadjaj.Load();
+            entities.Korisnik.Load();
         }
 
         private void frmDnevnik_Load(object sender, EventArgs e)
         {
-            dgvEvidencijaDnevnihAktivnosti.DataSource = entities.DnevnaAktivnost.Local;
-            dgvEvidencijaProvjeraZnanja.DataSource = entities.ProvjeraZnanja.Local;
-            dgvEvidencijaBiljeski.DataSource = entities.Biljeske.Local;
+            //dgvEvidencijaDnevnihAktivnosti.DataSource = entities.DnevnaAktivnost.Local;
+            //dgvEvidencijaProvjeraZnanja.DataSource = entities.ProvjeraZnanja.Local;
+            //dgvEvidencijaBiljeski.DataSource = entities.Biljeske.Local;
 
-            //var upit = from a in entities.DnevnaAktivnost.Local
-            //           from d in entities.Dogadjaj.Local
-            //           from k in entities.Korisnik.Local
-            //           where a.Dogadjaj_idDogadjaj == d.idDogadjaj && d.Korisnik_idKorisnik == korisnikID
-            //           select new
-            //           {
-            //               a.nazivaktivnosti,
-            //               a.opisaktivnosti,
-            //               a.datumobavljeneaktivnosti,
-            //               a.brojsamostalnihzahvata,
-            //               a.brojzahvatauznadzor
-            //           };
-            //dgvEvidencijaDnevnihAktivnosti.DataSource = upit.ToList();
+            var upitAktivnost = from a in entities.DnevnaAktivnost.Local
+                       join d in entities.Dogadjaj.Local on a.Dogadjaj_idDogadjaj equals d.idDogadjaj
+                       join k in entities.Korisnik.Local on d.Korisnik_idKorisnik equals korisnikID
+                       select new
+                       {
+                           Naziv_aktivnosti = a.nazivaktivnosti,
+                           Opis_aktivnosti = a.opisaktivnosti,
+                           Datum_obavljene_aktivnosti = a.datumobavljeneaktivnosti,
+                           Broj_samostalnih_zahvata = a.brojsamostalnihzahvata,
+                           Broj_zahvata_uz_nadzor = a.brojzahvatauznadzor
+                       };
+            dgvEvidencijaDnevnihAktivnosti.DataSource = upitAktivnost.Distinct().ToList();
+
+            var upitProvjera = from p in entities.ProvjeraZnanja.Local
+                                join d in entities.Dogadjaj.Local on p.Dogadjaj_idDogadjaj equals d.idDogadjaj
+                                join k in entities.Korisnik.Local on d.Korisnik_idKorisnik equals korisnikID
+                                select new
+                                {
+                                    Naziv_aktivnosti = p.datumprovjere,
+                                    Pitanja = p.pitanja,
+                                    Ocjena = p.ocjena,
+                                    Naziv_dogadaja = d.nazivdogadjaja,
+                                    Status_dogadaja = d.statusdogadjaja
+                                };
+            dgvEvidencijaProvjeraZnanja.DataSource = upitProvjera.Distinct().ToList();
+
+            var upitBiljeske = from b in entities.Biljeske.Local
+                               where b.Korisnik_idKorisnik == korisnikID
+                               select new
+                               {
+                                   Datum_biljeske = b.datumbiljeske,
+                                   Biljeska = b.biljeska
+                               };
+            dgvEvidencijaBiljeski.DataSource = upitBiljeske.Distinct().ToList();
 
             dgvEvidencijaDnevnihAktivnosti.AutoResizeColumns();
             dgvEvidencijaProvjeraZnanja.AutoResizeColumns();
